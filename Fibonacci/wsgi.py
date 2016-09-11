095b5492a20d75b5f7c7151d2b091c1c19460bf8
Harsh Bhardwaj <harshbhardwaj@Harshs-MacBook-Air.local>
2016-09-12 01:16:03 +0530

    updated

--- Fibonacci/wsgi.py
+++ Fibonacci/wsgi.py
@@ -13,8 +13,6 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Fibonacci.settings")
 
-application = get_wsgi_application()
+from dj_static import Cling
 
-# from whitenoise.django import DjangoWhiteNoise
-
-# application = DjangoWhiteNoise(application)
+application = Cling(get_wsgi_application())
