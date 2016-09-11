c728b23eb748c244492c810f794e106c899e8f11
Harsh Bhardwaj <harshbhardwaj@Harshs-MacBook-Air.local>
2016-09-11 15:12:15 +0530

    css and static files added

--- Fibonacci/settings.py
+++ Fibonacci/settings.py
@@ -14,6 +14,9 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_DIR = os.path.dirname(__file__)
+STATICFILES_DIRS = (os.path.join(PROJECT_DIR, 'static'),)
+print STATICFILES_DIRS
 
 
 # Quick-start development settings - unsuitable for production
@@ -117,5 +120,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, '../static')
 STATIC_URL = '/static/'
