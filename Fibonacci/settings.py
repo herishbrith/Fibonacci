35d273502b78d7bf307e70bb47842c8ce75f3002
Harsh Bhardwaj <harshbhardwaj@Harshs-MacBook-Air.local>
2016-09-11 16:39:35 +0530

    setting changed

--- Fibonacci/settings.py
+++ Fibonacci/settings.py
@@ -79,8 +79,6 @@ WSGI_APPLICATION = 'Fibonacci.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
