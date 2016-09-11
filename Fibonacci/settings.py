a00afbc19c3b03c42598084b73722b415439a79e
Harsh Bhardwaj <harshbhardwaj@Harshs-MacBook-Air.local>
2016-09-11 16:56:11 +0530

    changed files

--- Fibonacci/settings.py
+++ Fibonacci/settings.py
@@ -14,9 +14,6 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-PROJECT_DIR = os.path.dirname(__file__)
-STATICFILES_DIRS = (os.path.join(PROJECT_DIR, 'static'),)
-print STATICFILES_DIRS
 
 
 # Quick-start development settings - unsuitable for production
@@ -118,7 +115,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, '../static')
+STATICFILES_DIRS = os.path.join(BASE_DIR, '../static')
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
