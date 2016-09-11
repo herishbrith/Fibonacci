9f25c01b806b361c835a8418b68b9bff07f3ec7c
Harsh Bhardwaj <harshbhardwaj@Harshs-MacBook-Air.local>
2016-09-11 23:57:37 +0530

    updated

--- Fibonacci/settings.py
+++ Fibonacci/settings.py
@@ -110,10 +110,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'staticfiles')
 STATICFILES_DIRS = (
 
-	os.path.join(BASE_DIR, 'static'),
+	os.path.join(os.path.dirname(BASE_DIR), 'static'),
 )
 STATIC_URL = "/static/"
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
