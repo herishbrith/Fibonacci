3e0029ca1992aed56753884d4979dbb47756856a
Harsh Bhardwaj <harshbhardwaj@Harshs-MacBook-Air.local>
2016-09-11 17:53:34 +0530

    updated

--- Fibonacci/settings.py
+++ Fibonacci/settings.py
@@ -116,6 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+PROJECT_DIR = os.path.join(PROJECT_ROOT,'../Fibonacci')
 STATIC_ROOT= os.path.join(PROJECT_DIR,'staticfiles/')
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT,'static/'),
