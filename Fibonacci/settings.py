19c05e12d7f25a9fb977f3f83cab5a22c72a2c5c
Harsh Bhardwaj <harshbhardwaj@Harshs-MacBook-Air.local>
2016-09-11 16:50:27 +0530

    files changed

--- Fibonacci/settings.py
+++ Fibonacci/settings.py
@@ -26,9 +26,9 @@ print STATICFILES_DIRS
 SECRET_KEY = '3f&%xynp*zw7f*e8b5e2188anrt(gt^9-9()#82n6970c+vn+i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -120,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, '../static')
 STATIC_URL = '/static/'
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
