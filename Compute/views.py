bed0cf08d89efd4cdc16d59b9dcef406b19b1697
Harsh Bhardwaj <harshbhardwaj@Harshs-MacBook-Air.local>
2016-09-12 11:07:47 +0530

    updated

--- Compute/views.py
+++ Compute/views.py
@@ -1,24 +1,29 @@
 from django.shortcuts import render, redirect
 from django.views.decorators.csrf import csrf_exempt
 from Compute.forms import NumberForm
+from time import time
 
 @csrf_exempt
 def computeNthNumber(request):
 
 	if request.method == "GET":
 
+		# if the request method is GET, get a blank form
 		form = NumberForm()
 		return render(request, "Compute/Fibonacci.html", {"form": form})
 
 
 	elif request.method == "POST":
 
+		# if the request is POST, return the result
 		form = NumberForm(data=request.POST)
 
 		if form.is_valid():
 
 			data = form.data
 			pos = int(data["nthNumber"])
+			ordinal = lambda n: "%d%s" % (n,"tsnrhtdd"[(n/10%10!=1)*(n%10<4)*n%10::4])
+			start = time() # time to calculate the processing time
 			x, y, z = 0, 1, 1
 
 			for i in range(pos-1):
@@ -26,8 +31,8 @@ def computeNthNumber(request):
 				z = x + y
 				x = y
 				y = z
-
-			return render(request, "Compute/Fibonacci.html", {"result": z, "form": form})
+			stop = time() - start
+			return render(request, "Compute/Fibonacci.html", {"result": z, "form": form, "time": stop, "ordinal": ordinal(pos)})
 
 		else: return render(request, "Compute/Fibonacci.html", {"form": form})
 
